e320b1bbf540ac79e5cdf0aa177fe3a0b7384a21
vanelo <vanelo@users.noreply.github.com>
2021-11-08 16:48:18 -0300

    first commit

---
+++ source/dynamoDBPort.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const databaseManager = require('./dynamoDBConnector');
+
+module.exports.saveItem = async item => {
+    const params = {
+        TableName: process.env.GREETER_DYNAMODB_TABLE,
+        Item: item
+    };
+
+    return await databaseManager.saveItem(params);
+}
+
+module.exports.getItem = async name => {
+    const params = {
+        Key: {
+            name
+        },
+        TableName: process.env.GREETER_DYNAMODB_TABLE
+    };
+
+    return await databaseManager.getItem(params);
+}
---
+++ source/handler.js
@@ -0,0 +1,39 @@
+'use strict';
+const greeter = require('./greeter');
+
+module.exports.hello = async (event) => {
+  const name = event.queryStringParmaters && event.queryStringParmaters.name;
+  const greet = greeter.sayHello(name);
+
+	return {
+		statusCode: 200,
+		body: greet
+	};
+};
+
+module.exports.moi = async event => {
+	const name = event.queryStringParmaters  && event.queryStringParmaters.name;
+	const greet = await greeter.sayHelloAndRecord(name);
+
+	return {
+		statusCode: 200,
+		body: greet
+	}
+}
+
+module.exports.wasGreeted = async event => {
+	const name = event.queryStringParmaters && event.queryStringParmaters.name;
+	const result = await greeter.wasGreeted(name);
+
+	if(result === true){
+		return {
+			statusCode: 200,
+			body: 'Greet found'
+		};
+	}else{
+		return {
+			statusCode: 404,
+			body: 'Greet not found'
+		};
+	}
+}
